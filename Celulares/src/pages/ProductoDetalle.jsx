60fd0212ea3fb1535af32f9ba041749eb01b7583
aaronsele <48522418@est.ort.edu.ar>
2025-06-28 23:03:55 -0300

    QUE BUENOS QUE SOMOS

--- Celulares/src/pages/ProductoDetalle.jsx
+++ Celulares/src/pages/ProductoDetalle.jsx
@@ -1,29 +1,43 @@
 import { useParams } from 'react-router-dom';
 import { celulares } from '../data/data';
-import './ProductoDetalle.css'; 
+import { useState } from 'react';
+import { FaShoppingCart, FaCheck } from 'react-icons/fa';
+import './ProductoDetalle.css';
 
 function ProductoDetalle() {
   const { idCelular } = useParams();
   const celular = celulares.find(c => c.id === Number(idCelular));
-
-  console.log(idCelular);
+  const [agregado, setAgregado] = useState(false);
 
   if (!celular) {
     return <h2>Celular no encontrado 😢</h2>;
   }
 
+  const handleAgregar = () => {
+    setAgregado(true);
+    setTimeout(() => setAgregado(false), 1500);
+  };
+
   return (
     <div className="producto-detalle">
       <h1>{celular.nombre}</h1>
       <div className="producto-detalle-info">
-
         <div className="producto-detalle-fotos">
-        <img src={celular.fotos[0]} alt={`${celular.nombre}`} />
+          <img src={celular.fotos[0]} alt={`${celular.nombre}`} />
         </div>
 
         <div className="producto-detalle-texto">
           <p><strong>Descripción:</strong> {celular.descripcion}</p>
           <p><strong>Precio:</strong> ${celular.precio.toLocaleString()}</p>
+
+          <div className="add-to-cart">
+            <button onClick={handleAgregar} className={agregado ? 'animado' : ''}>
+              {agregado
+                ? <FaCheck className="cart check" />
+                : <FaShoppingCart className="cart" />}
+              <span>{agregado ? 'Agregado' : 'Agregar al carrito'}</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
