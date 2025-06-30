9da8e16783e7235d423e6cb8cbdf96545bcffc41
aaronsele <48522418@est.ort.edu.ar>
2025-06-30 08:16:01 -0300

    fin?

--- Celulares/src/components/Navbar.jsx
+++ Celulares/src/components/Navbar.jsx
@@ -1,42 +1,30 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import { marcas } from "../data/data";
 import { FaShoppingCart } from "react-icons/fa";
 import './Navbar.css';
 
 function Navbar() {
-  const [mostrarMenu, setMostrarMenu] = useState(false);
-
   return (
     <nav>
       <img src='/src/assets/logo.png' alt="Logo de Five GigaGuys" />
 
-      <Link to="/">Inicio</Link>{" | "}
-      <Link to="/quienes-somos">Quienes somos</Link>{" | "}
-      
-      <button onClick={() => setMostrarMenu(!mostrarMenu)}>
-        Productos +
-      </button>
+      <Link to="/">Inicio</Link>
+      <Link to="/quienes-somos">Quienes somos</Link>
 
-      {mostrarMenu && (
-        <>
-          <Link to="/productos">Ver todos</Link>{" | "}
+      <div className="dropdown">
+        <span className="dropdown-title">Productos</span>
+        <div className="dropdown-content">
+          <Link to="/productos">Ver todos</Link>
           {marcas.map((marca) => (
-            <Link
-              key={marca.id}
-              to={`/productos/${marca.id}`}
-            >
+            <Link key={marca.id} to={`/productos/${marca.id}`}>
               {marca.nombre}
             </Link>
           ))}
-        </>
-      )}
-      
-      {" | "}
-      <Link to="/contacto">Contacto</Link>{" | "}
-      <Link to="/consultas">Consultas</Link>{" | "}
-      
-      {/* Carrito */}
+        </div>
+      </div>
+
+      <Link to="/contacto">Contacto</Link>
+      <Link to="/consultas">Consultas</Link>
       <Link to="/carrito" className="icono-carrito" title="Ver carrito">
         <FaShoppingCart size={20} />
       </Link>
